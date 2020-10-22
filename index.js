79f31808e1e170e9ab0c941f4ef60990ecebe447
Erick Eduardo Petrucelli <erick.petrucelli@fatectq.edu.br>
2020-10-22 11:05:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import nedb from 'nedb'
+import rest from 'express-nedb-rest'
+
+const app = new express()
+const api = rest()
+
+const dbPosts = new nedb({ filename: 'posts.db', autoload: true })
+api.addDatastore('posts', dbPosts)
+
+const dbUsers = new nedb({ filename: 'users.db', autoload: true })
+api.addDatastore('users', dbUsers)
+
+const dbTodos = new nedb({ filename: 'todos.db', autoload: true })
+api.addDatastore('todos', dbTodos)
+
+app.use('/', api)
+
+app.listen(8080, () => console.log('Listening at http://localhost:8080'))
